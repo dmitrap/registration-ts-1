78a95181120a0e622a0e09c1479a5905d7005da5
maqweb <balikhinv@bk.ru>
2020-02-21 14:18:37 +0300

    added active-link style

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,10 +5,10 @@ import {NavLink} from "react-router-dom";
 const Menu: React.FC = () => {
     return (
         <div className="menu">
-            <NavLink to={'/login'}>Login</NavLink>
-            <NavLink to={'/registration'}>Registration</NavLink>
-            <NavLink to={'/recovery'}>Recovery Password</NavLink>
-            <NavLink to={'/profile'}>Profile</NavLink>
+            <NavLink activeClassName="activeLink" to={'/login'}>Login</NavLink>
+            <NavLink activeClassName="activeLink" to={'/registration'}>Registration</NavLink>
+            <NavLink activeClassName="activeLink" to={'/recovery'}>Recovery Password</NavLink>
+            <NavLink activeClassName="activeLink" to={'/profile'}>Profile</NavLink>
         </div>
     );
 };
